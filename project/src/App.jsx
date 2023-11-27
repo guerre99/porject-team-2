dd23784a6363cd9847f5e0a21e64d9457204780a
guerre99 <dgr761@gmail.com>
2023-11-27 19:15:35 +0100

    firs commit develop-david

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,11 @@
 import './styles.css'
+import Body from './components/body/body'
 
 function App() {
   return (
     <>
       <h1>Test</h1>
+      <Body />
     </>
   )
 }
---
+++ project/src/components/body/card.jsx
@@ -0,0 +1,48 @@
+import { Grid, Button } from '@mui/material'
+
+function Card() {
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+        sx={{
+          width: '60%',
+          mx: 'auto',
+          my: '2%',
+          backgroundColor: 'white',
+        }}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{
+            width: '70%',
+          }}
+        >
+          <p>Long link</p>
+          <p>Short link</p>
+        </Grid>
+        <Button
+          variant="contained"
+          sx={{
+            width: '20%',
+            height: '100%',
+            backgroundColor: '#2BD0D0',
+            '&:hover': {
+              backgroundColor: '#4B3F6B   ',
+            },
+          }}
+        >
+          Copy
+        </Button>
+      </Grid>
+    </>
+  )
+}
+
+export default Card
