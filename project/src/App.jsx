8d0b0d81836c12f7d2a9dfdb136165d25e6c75c2
petetes93 <alejandrolobatohaurie@gmail.com>
2023-11-27 20:31:42 +0100

    first commit developAlex

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,48 @@
 import './styles.css'
+import './components/footer/footer.css'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <>
       <h1>Test</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        maxime libero repellendus sapiente sequi neque eum ratione non
+        accusantium iure nostrum sunt, sit dicta vel, doloremque, obcaecati
+        necessitatibus similique ipsa. Ea, autem explicabo facere aut quod
+        inventore sit similique cupiditate numquam deleniti iusto labore nemo.
+        Labore voluptatibus assumenda aut neque necessitatibus quis aperiam,
+        atque sit voluptatum! Nobis rem explicabo maxime. Maxime minima
+        aspernatur mollitia fuga sint velit beatae similique facilis sed
+        doloribus commodi neque, labore rem, aut nihil nam! Odit amet minima
+        ullam perferendis! In, ex quae. Voluptatem, est. Reiciendis. Adipisci
+        repudiandae ipsam sapiente optio. Ipsam eligendi quo, possimus numquam,
+        illum quia eveniet consequatur nesciunt recusandae doloremque quos aut
+        dolore ipsum excepturi voluptatibus dicta debitis, exercitationem
+        impedit magni soluta ea. Eaque temporibus sed eum. Exercitationem unde
+        at minima rerum, libero repudiandae eum sunt illo, asperiores
+        consectetur eveniet neque ad nihil aliquid facere? In delectus ducimus
+        doloribus sapiente mollitia! Similique, velit! Corporis, obcaecati
+        excepturi molestias quidem autem laboriosam magni quas odit vero. Nam
+        perferendis fugiat vitae veritatis! Animi quasi accusantium perspiciatis
+        voluptas, cupiditate omnis nesciunt rem nam asperiores aut adipisci
+        reiciendis! Velit, numquam fuga error delectus amet a sint alias
+        cupiditate vero provident rem eveniet eligendi corporis omnis tempora
+        inventore neque dolor. Neque porro illo deleniti harum quos laborum
+        totam mollitia. Quisquam nam dolores eligendi maiores, minima laudantium
+        corporis pariatur atque dolorem sequi esse harum sapiente perspiciatis,
+        voluptatum sunt dolor nemo id placeat enim? Ipsum error accusamus, quod
+        illum eaque quibusdam. Perferendis mollitia, officiis voluptas
+        temporibus quasi dolores dolor illo possimus recusandae, laudantium ex
+        doloribus voluptatem ullam dignissimos? Harum culpa nostrum tempore
+        consequatur, iure dolore quod, recusandae, alias soluta architecto
+        laboriosam. Corporis dignissimos tempora veniam reprehenderit laboriosam
+        optio reiciendis odit ullam doloribus numquam quidem sapiente, molestiae
+        aut iure atque possimus ducimus eaque itaque exercitationem harum.
+        Consequuntur optio aspernatur hic architecto libero.
+      </p>
+      <Footer />
     </>
   )
 }
---
+++ project/src/components/footer/FooterLinks.jsx
@@ -0,0 +1,211 @@
+// FooterLinks.jsx
+
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
+import facebookIcon from './assets/icon-facebook.svg'
+import twitterIcon from './assets/icon-twitter.svg'
+import pinterestIcon from './assets/icon-pinterest.svg'
+import instagramIcon from './assets/icon-instagram.svg'
+
+function FooterLinks() {
+  return (
+    <Grid
+      container
+      justifyContent='center'
+      sx={{
+        backgroundColor: '#232127',
+        color: '#ffffff',
+        padding: '50px 0',
+        width: '100%',
+        position: 'relative',
+      }}
+    >
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography variant='h5'>Shortly</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography variant='h6'>Features</Typography>
+        <ul
+          sx={{
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+            textAlign: 'left',
+          }}
+        >
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Link Shortening
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Branded Links
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Analytics
+            </a>
+          </li>
+        </ul>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography variant='h6'>Resources</Typography>
+        <ul
+          sx={{
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+            textAlign: 'left',
+          }}
+        >
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Developers
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Support
+            </a>
+          </li>
+        </ul>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography variant='h6'>Company</Typography>
+        <ul
+          sx={{
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+            textAlign: 'left',
+          }}
+        >
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Our Team
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Careers
+            </a>
+          </li>
+          <li>
+            <a href='#' style={{ textDecoration: 'none', color: '#ffffff' }}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        justifyContent='flex-end'
+        alignItems='center'
+        sx={{
+          position: 'absolute',
+          top: '20px',
+          right: '0',
+          display: 'flex',
+        }}
+      >
+        <a
+          href='#'
+          style={{
+            textDecoration: 'none',
+            color: '#ffffff',
+            margin: '0 5px',
+          }}
+          className='social-icon'
+        >
+          <img
+            src={facebookIcon}
+            alt='Facebook Icon'
+            sx={{
+              width: '30px',
+              '&:hover': {
+                color: '#2BD0D0',
+              },
+            }}
+          />
+        </a>
+        <a
+          href='#'
+          style={{
+            textDecoration: 'none',
+            color: '#ffffff',
+            margin: '0 5px',
+          }}
+          className='social-icon'
+        >
+          <img
+            src={twitterIcon}
+            alt='Twitter Icon'
+            sx={{
+              width: '30px',
+              '&:hover': {
+                color: '#2BD0D0',
+              },
+            }}
+          />
+        </a>
+        <a
+          href='#'
+          style={{
+            textDecoration: 'none',
+            color: '#ffffff',
+            margin: '0 5px',
+          }}
+          className='social-icon'
+        >
+          <img
+            src={pinterestIcon}
+            alt='Pinterest Icon'
+            sx={{
+              width: '30px',
+              '&:hover': {
+                color: '#2BD0D0',
+              },
+            }}
+          />
+        </a>
+        <a
+          href='#'
+          style={{
+            textDecoration: 'none',
+            color: '#ffffff',
+            margin: '0 5px',
+          }}
+          className='social-icon'
+        >
+          <img
+            src={instagramIcon}
+            alt='Instagram Icon'
+            sx={{
+              width: '30px',
+              '&:hover': {
+                color: '#2BD0D0',
+              },
+            }}
+          />
+        </a>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default FooterLinks
