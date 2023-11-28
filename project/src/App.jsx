96dbdb26229ef4d89bf5418cc27a5dc605fb2b6d
guerre99 <dgr761@gmail.com>
2023-11-28 09:54:31 +0100

    add fonts

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,12 @@
 import './styles.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/700.css'
 
 import './components/footer/footer.css'
-import Header from "./components/header/header";
+import Header from './components/header/header'
 import Footer from './components/footer/footer'
 
 import Body from './components/body/body'
-
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Body />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
