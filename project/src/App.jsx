2d4b7bb01af4512f378358b7d1287ddbd82e85b3
petetes93 <alejandrolobatohaurie@gmail.com>
2023-11-28 09:57:25 +0100

    second commit developAlex

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,10 @@
 import './styles.css'
 
 import './components/footer/footer.css'
-import Header from "./components/header/header";
+import Header from './components/header/header'
 import Footer from './components/footer/footer'
 
 import Body from './components/body/body'
-
 
 function App() {
   return (
@@ -14,7 +13,7 @@ function App() {
       <Body />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
