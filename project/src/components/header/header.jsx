5fdb484b6b99fcc89431769ade66a64a8bbcc74a
MarcosLemus <marcoslemus1990@gmail.com>
2023-11-29 17:49:13 +0100

    third day first commit

--- project/src/components/header/header.jsx
+++ project/src/components/header/header.jsx
@@ -7,48 +7,55 @@ import myImage from "../../../../assets/illustration-working.svg";
 function Header() {
   return (
     <>
-      {/* <ButtonAppBar /> */}
-      <ResponsiveAppBar />
+      <header>
+        {/* <ButtonAppBar /> */}
+        <ResponsiveAppBar />
 
-      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={5} mb="8%">
         <Box
-          gridColumn="span 8"
-          sx={{
-            mt: "24%",
-            ml: "30%",
-          }}
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          gap={5}
+          mb="8%"
         >
-          <h1>More than just shorter links</h1>
-          <p>
-            Build your brand's recognition and get detailed insights on how your
-            links are performing.
-          </p>
-          <Button
-            variant="contained"
+          <Box
+            gridColumn="span 8"
             sx={{
-              backgroundColor: "#2BD0D0",
-              "&:hover": {
-                background: "#bff0f0",
-                boxShadow: "none",
-              },
-              borderRadius: "30px",
-              padding: "10px 20px",
-              mr: "18%",
-              fontFamily: "Poppins",
-              fontWeight: "bold",
-              boxShadow: "none",
+              mt: "24%",
+              ml: "30%",
             }}
           >
-            Get Started
-          </Button>
-        </Box>
+            <h1>More than just shorter links</h1>
+            <p>
+              Build your brand's recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#2BD0D0",
+                "&:hover": {
+                  background: "#bff0f0",
+                  boxShadow: "none",
+                },
+                borderRadius: "30px",
+                padding: "10px 20px",
+                mr: "18%",
+                fontFamily: "Poppins",
+                fontWeight: "bold",
+                boxShadow: "none",
+              }}
+            >
+              Get Started
+            </Button>
+          </Box>
 
-        <Box>
-          <ImageList sx={{ transform: "translate(20%, 40%)" }}>
-            <img src={myImage} alt="hola" width="500px" />
-          </ImageList>
+          <Box>
+            <ImageList sx={{ transform: "translate(20%, 40%)" }}>
+              <img src={myImage} alt="hola" width="500px" />
+            </ImageList>
+          </Box>
         </Box>
-      </Box>
+      </header>
     </>
   );
 }
