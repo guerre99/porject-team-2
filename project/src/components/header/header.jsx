853bed81e97dc3af8a30725b02ea436adb2b23ab
petetes93 <alejandrolobatohaurie@gmail.com>
2023-11-29 18:42:47 +0100

    arreglado responsive para middlebody

--- project/src/components/header/header.jsx
+++ project/src/components/header/header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react'
 // import ButtonAppBar from "./navbar";
-import ResponsiveAppBar from "./navbar_responsive";
-import { Box, Button, ImageList, Grid } from "@mui/material";
-import myImage from "../../../../assets/illustration-working.svg";
+import ResponsiveAppBar from './navbar_responsive'
+import { Box, Button, ImageList, Grid } from '@mui/material'
+import myImage from '../../../../assets/illustration-working.svg'
 
 function Header() {
   return (
@@ -10,12 +10,12 @@ function Header() {
       {/* <ButtonAppBar /> */}
       <ResponsiveAppBar />
 
-      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={5} mb="8%">
+      <Box display='grid' gridTemplateColumns='repeat(12, 1fr)' gap={5} mb='8%'>
         <Box
-          gridColumn="span 8"
+          gridColumn='span 8'
           sx={{
-            mt: "24%",
-            ml: "30%",
+            mt: '24%',
+            ml: '30%',
           }}
         >
           <h1>More than just shorter links</h1>
@@ -24,33 +24,33 @@ function Header() {
             links are performing.
           </p>
           <Button
-            variant="contained"
+            variant='contained'
             sx={{
-              backgroundColor: "#2BD0D0",
-              "&:hover": {
-                background: "#bff0f0",
-                boxShadow: "none",
+              backgroundColor: '#2BD0D0',
+              '&:hover': {
+                background: '#bff0f0',
+                boxShadow: 'none',
               },
-              borderRadius: "30px",
-              padding: "10px 20px",
-              mr: "18%",
-              fontFamily: "Poppins",
-              fontWeight: "bold",
-              boxShadow: "none",
+              borderRadius: '30px',
+              padding: '10px 20px',
+              mr: '18%',
+              fontFamily: 'Poppins',
+              fontWeight: 'bold',
+              boxShadow: 'none',
             }}
           >
             Get Started
           </Button>
         </Box>
 
-        <Box>
-          <ImageList sx={{ transform: "translate(20%, 40%)" }}>
-            <img src={myImage} alt="hola" width="500px" />
+        {/* <Box>
+          <ImageList sx={{ transform: 'translate(20%, 40%)' }}>
+            <img src={myImage} alt='hola' width='500px' />
           </ImageList>
-        </Box>
+        </Box> */}
       </Box>
     </>
-  );
+  )
 }
 
-export default Header;
+export default Header
