777cbead6b913ed41dac1ed69bf42787bd19eea3
petetes93 <alejandrolobatohaurie@gmail.com>
2023-11-30 18:18:41 +0100

    dia 4: funcionalidad del boton y hover al clickar

--- project/src/components/body/card.jsx
+++ project/src/components/body/card.jsx
@@ -1,13 +1,26 @@
 import { Grid, Button } from '@mui/material'
+import React, { useState } from 'react'
 
 function Card({ originalURL, shortURL }) {
+  const [copied, setCopied] = useState(false)
+
+  const copiarAlPortapapeles = (textToCopy) => {
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        setCopied(true)
+        setTimeout(() => setCopied(false), 200)
+      })
+      .catch((err) => console.error('Error copying to clipboard:', err))
+  }
+
   return (
     <>
       <Grid
         container
-        direction="row"
-        justifyContent="space-around"
-        alignItems="center"
+        direction='row'
+        justifyContent='space-around'
+        alignItems='center'
         sx={{
           width: '70%',
           mx: 'auto',
@@ -17,9 +30,9 @@ function Card({ originalURL, shortURL }) {
       >
         <Grid
           container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
+          direction='row'
+          justifyContent='space-between'
+          alignItems='center'
           sx={{
             width: '70%',
             wordBreak: 'break-all',
@@ -29,17 +42,19 @@ function Card({ originalURL, shortURL }) {
           <p style={{ color: '#2BD0D0' }}>{shortURL}</p>
         </Grid>
         <Button
-          variant="contained"
+          variant='contained'
+          onClick={() => copiarAlPortapapeles(shortURL)}
           sx={{
             width: '20%',
             height: '100%',
-            backgroundColor: '#2BD0D0',
+            backgroundColor: copied ? '#4B3F6B' : '#2BD0D0',
             '&:hover': {
-              backgroundColor: '#4B3F6B   ',
+              backgroundColor: copied ? '#4B3F6B' : '#2BD0D0', // problemas con el color del hover por default, asi que implemento este color
             },
           }}
         >
-          Copy
+          {copied && 'Copied'}
+          {!copied && 'Copy'}
         </Button>
       </Grid>
     </>
